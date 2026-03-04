fa4229e16a4d0a5b9f17d1b795c5e3175756c45f
Jy4058350 <j_yamabe@mac.com>
2026-03-04 14:20:27 +0900

    listの学習

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-height_m = float(input("Enter your height in meters (e.g. 1.65):"))
-height_cm = height_m*100
-print(f"Your height is {height_cm} cm.")
-print(type(height_m),type(height_cm))
+# name = input("Enter your name: ").strip()
+# age = int(input("Enter your age: ").strip())
+# height_m = float(input("Enter your height (m): ").strip())
+
+# answer = input("Are you a student? (y/n):").strip().lower()
+# is_student = answer == "y"
+
+# print("\n--- Values ---")
+# print(name, age, height_m, is_student)
+
+# print("\n--- Types ---")
+# print(type(name), type(age), type(height_m), type(is_student))
+
+# if is_student:
+#     print("Student mode ON")
+# else:
+#     print("Student mode OFF")
+
+
+fruits = []
+
+for i in range(3):
+    fruit = input(f"Fruit {i+1}: ").strip()
+    fruits.append(fruit)
+
+print("\nYour fruits:")
+for fruit in fruits:
+    print("-", fruit)
+
+print(f"Total: {len(fruits)} items")
