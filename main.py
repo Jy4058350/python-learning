fed9891b5b305897afa5e9cd5a2f816e80e1ce90
Jy4058350 <j_yamabe@mac.com>
2026-03-04 13:09:21 +0900

    float 関数をつかう。文字列を小数点ありの数字に変換する

--- main.py
+++ main.py
@@ -1,38 +1,4 @@
-def get_name():
-  while True:
-    name = input("what is your name?").strip()
-    if name != "":
-      return name
-    print("Name is required. Please try again.")
-
-name = get_name()
-print(f"Hello, {name}!")
-
-
-def get_age():
-  while True:
-    age_text = input("How old are you?").strip()
-    if age_text.isdigit():
-      age = int(age_text)
-      if age >= 0:
-        return age
-    print("Age must be a number 0 or higher. Please try again.")
-
-age = get_age()
-
-def is_adult(age):
-    return age >= 18
-
-def main():
-   name = get_name()
-   print(f"Hello, {name}!")
-
-   age = get_age()
-   if is_adult(age):
-      print("You are an adult.")
-   else:
-     print("You are a minor.")
-
-
-if __name__ == "__main__":
-   main()
+height_m = float(input("Enter your height in meters (e.g. 1.65):"))
+height_cm = height_m*100
+print(f"Your height is {height_cm} cm.")
+print(type(height_m),type(height_cm))
